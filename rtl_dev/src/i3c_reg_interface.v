34cc29f45d1790fcbec2e4d8e8bb41de0be7d3be
Suhan Rai K <149513511+SuhanRai@users.noreply.github.com>
2026-03-30 15:04:55 +0530

    Remove unused signals and clean up code

--- rtl_dev/src/i3c_reg_interface.v
+++ rtl_dev/src/i3c_reg_interface.v
@@ -19,15 +19,6 @@ module i3c_reg_interface (
   output reg         Rx_rd_en,
   input  wire [7:0]  Rx_rdata,
 
-  input  wire        SDR_DONE,
-  input  wire        DAA_DONE,
-  input  wire        SDR_BUSY,
-  input  wire        DAA_BUSY,
-  input  wire        DAA_ERROR,
-  input  wire        SDR_ERROR,
-  input  wire        nak,
-
-  input  wire [6:0]  daa_dyn_addr,
 
   output reg         cmd_start,
   output reg  [1:0]  cmd_type,
@@ -37,17 +28,11 @@ module i3c_reg_interface (
   output reg         cmd_dir
 );
 
-reg [31:0] CONFIG;
 reg [31:0] CTRL;
-reg [6:0]  DYNADDR;
-reg [7:0]  STATUS;
 
 always @(posedge clk or negedge rst_n) begin
   if (!rst_n) begin
-    CONFIG     <= 0;
     CTRL       <= 0;
-    STATUS     <= 0;
-    DYNADDR    <= 0;
     Tx_wr_en   <= 0;
     Rx_rd_en   <= 0;
     cmd_start  <= 0;
@@ -58,14 +43,13 @@ always @(posedge clk or negedge rst_n) begin
     cmd_ccc    <= 0;
     rd_data    <= 0;
     r_data     <= 0;
+    
   end else begin
     Tx_wr_en  <= 0;
     Rx_rd_en  <= 0;
     cmd_start <= 0;
     if (wr_en) begin
       case (addrs)
-        `REG_CONFIG:
-          CONFIG <= w_reg_data;
         `REG_CTRL: begin
           CTRL       <= w_reg_data;
           cmd_start <= w_reg_data[31];
@@ -83,25 +67,13 @@ always @(posedge clk or negedge rst_n) begin
     end
     if (rd_en) begin
       case (addrs)
-        `REG_CONFIG:   rd_data <= CONFIG;
-        `REG_STATUS:   rd_data <= {27'd0, STATUS};
-        `REG_CTRL:     rd_data <= CTRL;
-        `REG_DYNADDR:  rd_data <= {25'd0, DYNADDR};
         `REG_RDATAB: begin
-          Rx_rd_en <= 1'b1;
-          r_data   <= Rx_rdata;
+          Rx_rd_en  <= 1'b1;
+          r_data   <= Rx_rdata; 
         end
       endcase
     end
-    STATUS[0] <= SDR_DONE;
-    STATUS[1] <= DAA_DONE;
-    STATUS[2] <= SDR_BUSY;
-    STATUS[3] <= DAA_BUSY;
-    STATUS[4] <= SDR_ERROR;
-    STATUS[5] <= DAA_ERROR;
-    STATUS[6] <= nak;
-    if (DAA_DONE)
-      DYNADDR <= daa_dyn_addr;
+
   end
 end
 
